63fae266a7d16386b58a0e940887f240c07b677a
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-06 16:52:09 +0200

    understanding variables

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let js = "amazing";
-if (js === "amazing") alert('Javascript is Fun!');
 console.log("Using console.log - builds bridge between script and developer console in the browser ");
+console.log(2);
+console.log("Eugenia");
+let myAge = 31;
+let firstName = "Bob";
+console.log(firstName);
+console.log("camelCase, variable name cannot start with number!, but can have underscore and dollar sign. have to be descriptive.")
