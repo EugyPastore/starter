f378a44ceeb4106beb9e9a92138d4969c0020a77
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-07 09:44:20 +0200

    let/const/var differences. Good practice to have as little mutations as possible, always declare variables, var is function scoped, let is block scoped. Use const over let.

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
+// mutate age variable = reassign
+// GOOD PRACTICE : to have as little mutations as possible, so less bugs appear!
+// By default always use CONST as much as possible
+// var similar to let; var was used prior ES6;
+// var is function scoped.
+// ALWAYS DECLARE variables! surname = "Pastore" --> not good!
+var job = "programmer";
+job = "full-stack developer";
+// let is block scoped
+let age = 31;
+age = 32;
+// const not supposed to change in the future
+const birthYear = 1990;
+// const variables cannot be empty! error: missing initializer
+
 // variable conventions
+// let js = "amazing";
+// console.log("Using console.log - builds bridge between script and developer console in the browser ");
+// console.log(2);
+// console.log("Eugenia");
+// let myAge = 31;
+// let firstName = "Bob";
+// console.log(firstName);
+// console.log("camelCase, variable name cannot start with number!, but can have underscore and dollar sign. have to be descriptive.")
 
-let js = "amazing";
-console.log("Using console.log - builds bridge between script and developer console in the browser ");
-console.log(2);
-console.log("Eugenia");
-let myAge = 31;
-let firstName = "Bob";
-console.log(firstName);
-console.log("camelCase, variable name cannot start with number!, but can have underscore and dollar sign. have to be descriptive.")
+// // boolean
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof true);
 
-// boolean
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-console.log(typeof true);
+// // dynamic typing. changing values
+// javaScriptIsFun = "Yes!";
+// console.log(typeof javaScriptIsFun);
 
-// dynamic typing. changing values
-javaScriptIsFun = "Yes!";
-console.log(typeof javaScriptIsFun);
+// // undefined variable
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// // reassign
 
-// undefined variable
-let year;
-console.log(year);
-console.log(typeof year);
-// reassign
+// year = 1990;
+// console.log(typeof year);
 
-year = 1990;
-console.log(typeof year);
-
-// null- type of undefined
-console.log(typeof null)
+// // null- type of undefined
+// console.log(typeof null)
