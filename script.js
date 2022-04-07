25fa92984bc3501d1e39784e6f44838e14f78157
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-07 09:33:00 +0200

    Javascript variables

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// variable conventions
+
 let js = "amazing";
 console.log("Using console.log - builds bridge between script and developer console in the browser ");
 console.log(2);
@@ -6,3 +8,24 @@ let myAge = 31;
 let firstName = "Bob";
 console.log(firstName);
 console.log("camelCase, variable name cannot start with number!, but can have underscore and dollar sign. have to be descriptive.")
+
+// boolean
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+
+// dynamic typing. changing values
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+// undefined variable
+let year;
+console.log(year);
+console.log(typeof year);
+// reassign
+
+year = 1990;
+console.log(typeof year);
+
+// null- type of undefined
+console.log(typeof null)
