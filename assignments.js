b95fe9f95e5dde6dacd2a24bd541e21c196672e8
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-08 15:31:55 +0200

    final push

--- assignments.js
+++ assignments.js
@@ -39,8 +39,7 @@
 // 	console.log(`No team wins trophy.👐🏾`)
 // }
 
-//challange 4
-const bill = 430;
-const tip = bill >= 50 && bill >= 300 ? bill * 15 / 100 : bill * 20 / 100;
-
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+// //challange 4
+// const bill = 430;
+// const tip = bill >= 50 && bill >= 300 ? bill * 15 / 100 : bill * 20 / 100;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
