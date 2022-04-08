e385098f0cf399d8b08da6150619ddaa738d05a7
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-08 15:12:02 +0200

    challenge 4 solve. ternary operator

--- script.js
+++ script.js
@@ -124,30 +124,50 @@
 // prompt("What's your favourite number ?")
 //logical operators AND &&, || OR, !=opposite
 //switch statement
-const day = "monday";
+// const day = "wednesday";
 
-if (day === "monday") {
-	console.log('Plan course structure');
-	console.log("Go to coding meet up");
-} else if (day === "tuesday") {
-	console.log("Prepare theory videos");
-} else if (day === "wednesday") {
+// if (day === "monday") {
+// 	console.log('Plan course structure');
+// 	console.log("Go to coding meet up");
+// } else if (day === "tuesday") {
+// 	console.log("Prepare theory videos");
+// } else if (day === "wednesday") {
 
-} else if (day === "thursday") {
-	console.log("Write code examples");
-} else if (day === "friday") {
-	console.log("Record videos")
-} else if (day === "saturday") {
+// } else if (day === "thursday") {
+// 	console.log("Write code examples");
+// } else if (day === "friday") {
+// 	console.log("Record videos")
+// } else if (day === "saturday") {
 
-} else if (day === "sunday") {
-	console.log("enjoy weekend")
-} else {
-	console.log("Not a valid day")
-};
+// } else if (day === "sunday") {
+// 	console.log("enjoy weekend")
+// } else {
+// 	console.log("Not a valid day")
+// };
 
-//SWITCH STATEMENT
-switch (day) {
-	case "monday":
-		console.log('Plan course structure');
-		console.log("Go to coding meet up");
-}
+// //SWITCH STATEMENT
+// switch (day) {
+// 	case "monday":
+// 		console.log('Plan course structure');
+// 		console.log("Go to coding meet up");
+// 		break;
+// 	case "tuesday":
+// 		console.log("Prepare theory videos");
+// 		break;
+// 	case "wednesday":
+// 		break;
+// 	case "thursday":
+// 		console.log("Write code examples");
+// 		break;
+// 	case "friday":
+// 		console.log("Record videos")
+// 		break;
+// 	case "saturday":
+// 		break;
+// 	case "sunday":
+// 		console.log("enjoy weekend")
+// 		break;
+// 	default:
+// 		console.log("Not a valid day")
+// 		break;
+// }
