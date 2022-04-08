1342dc3ef84281423868891d002d7f14e1608fd2
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-04-08 13:50:47 +0200

    switch statement is like if/else stamement, it is less used, but requires less repetitive coding. we need to add break in case and also default

--- assignments.js
+++ assignments.js
@@ -25,16 +25,16 @@
 
 //3rd challange
 
-const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106) / 3;
-const scoreMin = 100;
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
+// const scoreMin = 100;
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= scoreMin) {
-	console.log(`Dolphins won! 🎉`)
-} else if (scoreDolphins < scoreKoalas && scoreKoalas >= scoreMin) {
-	console.log(`Koalas won!🎊`)
-} else if (scoreDolphins === scoreKoalas && scoreDolphins >= scoreMin && scoreKoalas >= scoreMin) {
-	console.log(`Same score.🤝`)
-} else {
-	console.log(`No team wins trophy.👐🏾`)
-}
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= scoreMin) {
+// 	console.log(`Dolphins won! 🎉`)
+// } else if (scoreDolphins < scoreKoalas && scoreKoalas >= scoreMin) {
+// 	console.log(`Koalas won!🎊`)
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= scoreMin && scoreKoalas >= scoreMin) {
+// 	console.log(`Same score.🤝`)
+// } else {
+// 	console.log(`No team wins trophy.👐🏾`)
+// }
--- script.js
+++ script.js
@@ -123,3 +123,31 @@
 // prompt is likle gets.chomp in ruby
 // prompt("What's your favourite number ?")
 //logical operators AND &&, || OR, !=opposite
+//switch statement
+const day = "monday";
+
+if (day === "monday") {
+	console.log('Plan course structure');
+	console.log("Go to coding meet up");
+} else if (day === "tuesday") {
+	console.log("Prepare theory videos");
+} else if (day === "wednesday") {
+
+} else if (day === "thursday") {
+	console.log("Write code examples");
+} else if (day === "friday") {
+	console.log("Record videos")
+} else if (day === "saturday") {
+
+} else if (day === "sunday") {
+	console.log("enjoy weekend")
+} else {
+	console.log("Not a valid day")
+};
+
+//SWITCH STATEMENT
+switch (day) {
+	case "monday":
+		console.log('Plan course structure');
+		console.log("Go to coding meet up");
+}
